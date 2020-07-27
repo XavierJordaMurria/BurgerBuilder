add3261e43f168dca01ddb93fd02228f8f2d8fae
Xavier Jorda <jorda.murria.xavier@gmail.com>
2020-07-27 08:07:54 +0200

    Creating Navigation Items BurguerBuilder and checkout toolbar

--- src/components/Navigation/ToolBar/ToolBar.js
+++ src/components/Navigation/ToolBar/ToolBar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import classes from './ToolBar.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const ToolBar = (props) => {
     return (
         <header className={classes.ToolBar}>
             <div>Menu</div>
             <Logo/>
-            <nav>...</nav>
+            <nav><NavigationItems/></nav>
         </header>
     );
 }
